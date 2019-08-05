dba50d7f5643a44739224f524e4497ef57319ff5
Alabi johnson <abiodunalabijohn@gmail.com>
2019-08-05 15:03:28 +0100

    updated

---
+++ resources/js/index.js
@@ -0,0 +1,20 @@
+
+
+// Select all links with hashes
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.section__project').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
